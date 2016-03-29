809c1437c5acfdc8f36012420cf865dca049b696
Joseph Blue <joebluems@yahoo.com>
2016-03-28 21:59:41 -0400

    loading model-building files
    DNN - deep neural net with R+H2O
    CNN - convolutional NN with Theano
    RNN - recurrent NN with Torch+Lua

---
+++ CNN/src/run.py
@@ -0,0 +1,24 @@
+#!/Users/jblue/anaconda2/bin/python
+# export DYLD_FALLBACK_LIBRARY_PATH=/Users/jblue/anaconda2/lib/
+## note: I had the best luck with Anaconda running theano - the export statement above was necessary to get my code to run
+import datetime
+import network3
+from network3 import Network
+from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+training_data, validation_data, test_data = network3.load_data_shared()
+mini_batch_size = 10
+
+print "starting: ",datetime.datetime.now()
+
+net = Network([
+        ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28), 
+                      filter_shape=(20, 1, 5, 5), 
+                      poolsize=(2, 2)),
+        FullyConnectedLayer(n_in=20*12*12, n_out=100),
+        SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
+
+net.SGD(training_data, 60, mini_batch_size, 0.1, 
+            validation_data, test_data) 
+
+print "finished: ",datetime.datetime.now()
+
